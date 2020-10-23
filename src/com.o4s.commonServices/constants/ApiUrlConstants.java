ad1a0166f3965f733f6f7fc64f8c19d18667fa2c
Radha Krishna <radha.singh@wakefit.co>
2020-10-23 23:32:41 +0530

    init api automation

---
+++ src/com.o4s.commonServices/constants/ApiUrlConstants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class ApiUrlConstants {
+    public static String baseUrlStaging="http://localhost:3000";
+    public static String baseUrlTest="http://localhost:3000";
+}
